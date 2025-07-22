31557959e393332aa47591dda05fd675a63487a9
madushan10 <mnmnjith@gmail.com>
2025-07-22 08:45:23 +0530

    header update 8

--- next.config.mjs
+++ next.config.mjs
@@ -26,8 +26,20 @@ const nextConfig = {
         { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
         { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains; preload' },
         { key: 'X-Content-Type-Options', value: 'nosniff' },
-        { key: 'Content-Security-Policy', value:
-         "default-src 'self'; frame-src 'self' https://dms.genaitech.dev; base-uri 'self'; object-src 'none'; connect-src 'self' https://dms.genaitech.dev; img-src 'self' data: https:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https:; font-src 'self' https: data:; frame-ancestors 'self';"},
+        {
+          key: 'Content-Security-Policy',
+          value:
+            "default-src 'self'; " +
+            "base-uri 'self'; " +
+            "object-src 'none'; " +
+            "connect-src 'self' https://dms.genaitech.dev; " +
+            "img-src 'self' data: https:; " +
+            "script-src 'self' 'unsafe-inline'; " +
+            "style-src 'self' 'unsafe-inline' https:; " +
+            "font-src 'self' https: data:; " +
+            "frame-src https://dms.genaitech.dev; " +
+            "frame-ancestors 'none';"
+        },
         { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
       ]
     }
